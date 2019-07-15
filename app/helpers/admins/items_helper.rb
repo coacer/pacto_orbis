14a8eaa433bf18c5c2835503ff07d869eb698f10
coacer <coacer728h23@icloud.com>
2019-07-15 14:59:20 +0900

    Generate items controller

---
+++ app/helpers/admins/items_helper.rb
@@ -0,0 +1,2 @@
+module Admins::ItemsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
   namespace :admins do
-    get 'genres/index'
-    get 'genres/edit'
+    resources :items
+    resources :artists, only: [:index, :edit, :create, :update, :destroy]
+    resources :labels, only: [:index, :edit, :create, :update, :destroy]
+    resources :genres, only: [:index, :edit, :create, :update, :destroy]
   end
-  namespace :admins do
-    get 'labels/index'
-    get 'labels/edit'
-  end
-  namespace :admins do
-    get 'artists/index'
-    get 'artists/edit'
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
