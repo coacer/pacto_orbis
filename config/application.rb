c3c624a57df5e685ff218c7260ae3b25b4d3c262
coacer <coacer728h23@icloud.com>
2019-07-17 17:39:33 +0900

    Refactoring to i18n

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module PactoOrbis
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.generators do |generator|
       generator.test_framework :rspec,
