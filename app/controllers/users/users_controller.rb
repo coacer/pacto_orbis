1849b6e77fb9044372af65b42f302dff8e80b139
coacer <coacer728h23@icloud.com>
2019-07-22 20:24:27 +0900

    Add error message and modify locale file

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -8,6 +8,12 @@ class Users::UsersController < ApplicationController
   end
 
   def update
+    if current_user.update(user_params)
+      flash[:success] = "ユーザー情報を編集しました"
+      redirect_to users_users_url
+    else
+      render :edit
+    end
   end
 
   def unsubscribe_confirm
@@ -15,4 +21,11 @@ class Users::UsersController < ApplicationController
 
   def unsubscribe
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :kana_first_name,
+                                   :kana_last_name, :postal_code, :address, :phone,
+                                   :email)
+    end
 end
