100ae98f229560b7d5a6a62fe9443a6c2ad39b9e
coacer <coacer728h23@icloud.com>
2019-07-24 17:16:11 +0900

    Finish address model validation and correspond to jpostal

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,33 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  before_validation :set_address
+  attr_accessor :prefecture_name, :city, :street
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :postal_code, presence: true, length: { in: 5..9 }, format: { with: /\A\w+\z/ }
+  validates :address, presence: true
+  validates :prefecture_name, presence: true
+  validates :city, presence: true
+  validates :street, presence: true
+
+  def read_postal_code
+    postal_code[0, 3] + "-" + postal_code[3, 4]
+  end
+
+  def prefecture_name
+    @prefecture_name || self.address.split[0] if self.address.present?
+  end
+
+  def city
+    @city || self.address.split[1] if self.address.present?
+  end
+
+  def street
+    @street || self.address.split[2] if self.address.present?
+  end
+
+  def set_address
+    self.address = [@prefecture_name, @city, @street].join(' ')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
+  has_many :addresses
 
   before_validation :set_address
   attr_accessor :prefecture_name, :city, :street
