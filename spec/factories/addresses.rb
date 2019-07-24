5d5e5f9b6fa85bc5a463775490356ec31e98ba70
coacer <coacer728h23@icloud.com>
2019-07-24 17:08:07 +0900

    Add address model spec

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
+  postal_code = Faker::Number.number(7)
+  address = Gimei.address
+  street = Faker::Number.number(4) + '-' + Faker::Number.number(3)
+
   factory :address do
-    name { "MyString" }
-    postal_code { "MyString" }
-    address { "MyString" }
-    user { nil }
+    name { "山田 太郎" }
+    postal_code { postal_code }
+    prefecture_name { address.prefecture.kanji }
+    city { address.city.kanji }
+    street { street }
+    association :user
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when a address is valid" do
+    it "is valid with all data" do
+      address = build(:address)
+      expect(address).to be_valid
+    end
+  end
+
+  context "when a address is invalid" do
+    it "is invalid with blank name" do
+      address = build(:address, name: "   ")
+      expect(address).to_not be_valid
+    end
+
+    it "is invalid with too long name" do
+      address = build(:address, name: "a" * 31)
+      expect(address).to_not be_valid
+    end
+
+    it "is invalid with blank postal_code" do
+      address = build(:address, postal_code: "   ")
+      expect(address).to_not be_valid
+    end
+
+    it "is invalid with postal_code hiragana and katakana" do
+      address_with_hiragana = build(:address, postal_code: "ひらがな")
+      address_with_katakana = build(:address, postal_code: "カタカナ")
+      expect(address_with_hiragana).to_not be_valid
+      expect(address_with_katakana).to_not be_valid
+    end
+
+    it "is invalid with too short postal_code" do
+      address = build(:address, postal_code: "0" * 4)
+      expect(address).to_not be_valid
+    end
+
+    it "is invalid with too long postal_code" do
+      address = build(:address, postal_code: "0" * 10)
+      expect(address).to_not be_valid
+    end
+
+    it "is invalid without user_id" do
+      address = build(:address, user_id: nil)
+      expect(address).to_not be_valid
+    end
+  end
 end
