6ba5a5c2ed270ddd49896bb5b4dd44b1432ce1c5
coacer <coacer728h23@icloud.com>
2019-11-04 15:13:08 +0900

    Setup for deploy

--- Gemfile
+++ Gemfile
@@ -80,5 +80,10 @@ group :test do
   gem 'chromedriver-helper'
 end
 
+group :production do
+  gem 'mysql2'
+  gem 'dotenv-rails'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/puma.rb
+++ config/puma.rb
@@ -32,3 +32,25 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+rails_root = Dir.pwd
+pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+stdout_redirect(
+  File.join(rails_root, 'log', 'puma.log'),
+  File.join(rails_root, 'log', 'puma-error.log'),
+  true
+)
+# 本番環境のみデーモン起動
+if Rails.env.production?
+    pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+    state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+    stdout_redirect(
+      File.join(rails_root, 'log', 'puma.log'),
+      File.join(rails_root, 'log', 'puma-error.log'),
+      true
+    )
+    # デーモン
+    daemonize
+end
